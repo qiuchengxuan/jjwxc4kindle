742a4898b929c019f9e352f041988d21cbc36931
qiuchengxuan <qiuchengxuan@gmail.com>
2017-02-01 12:19:44 +0800

    support login

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jjwxc4kindle
-一个适合kindle阅读晋江的小程序。
+为kindle阅读晋江做了些许页面优化的小应用。示例程序运行在[jjwxc4kindle](http://jjwxc4kindle.daoapp.io)
+目前支持账户登录，不过在晋江内购买章节什么的还没有试过会怎样
+此应用支持docker部署
--- jjwxc4kindle.py
+++ jjwxc4kindle.py
@@ -3,7 +3,7 @@
 import requests
 from lxml.builder import E
 from lxml import etree
-from flask import Flask, request, render_template
+from flask import *
 
 app = Flask(__name__)
 
@@ -14,29 +14,43 @@ def make_header():
 def to_utf8_content(content):
     return content.decode('gbk').replace('gb2312', 'utf-8')
 
-@app.route('/my/<path:path>')
+@app.route('/login/', methods=['POST'])
+def login():
+    user = request.form.get('username')
+    passwd = request.form.get('password')
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {'loginmode': 'jjwxc', 'loginname': user, 'loginpass': passwd}
+    r = requests.post('http://m.jjwxc.net/login/wapLogin', data=data,
+                      headers=headers, allow_redirects=False)
+    resp = redirect('/')
+    for key, value in r.cookies.items():
+        resp.set_cookie(key, value)
+    return resp
+
 @app.route('/')
-def index(path=None):
-    _ = path
-    url = 'http://m.jjwxc.net/' + request.path
-    r = requests.get(url, make_header())
-    return to_utf8_content(r.content)
+def index():
+    url = 'http://m.jjwxc.net/'
+    r = requests.get(url, make_header(), cookies=session)
+    body = etree.HTML(r.content.decode('gbk')).find('body')
+    content = ''.join([etree.tostring(c) for c in body.getchildren()])
+    return render_template('index.html', content=content)
 
 @app.route('/wap/<path:path>')
 @app.route('/images/<path:path>')
 @app.route('/favicon.ico')
-def raw(path=None):
+def static_file(path=None):
     _ = path
     url = 'http://m.jjwxc.net/' + request.path
-    return requests.get(url, make_header()).content
+    return requests.get(url, make_header(), cookies=session).content
 
 @app.route('/book2/<path:path>')
 def book_page(path):
+    url = 'http://m.jjwxc.net/book2/' + path
     if '/' not in path or path.split('/', 1)[1] is None:
-        r = requests.get('http://m.jjwxc.net/book2/' + path, make_header())
+        r = requests.get(url, make_header(), cookies=session)
         return to_utf8_content(r.content)
 
-    r = requests.get('http://m.jjwxc.net/book2/' + path, make_header())
+    r = requests.get(url, make_header(), cookies=session)
     book, chapter = path.split('/')[:2]
     dom = etree.HTML(r.content.decode('gbk'))
     content = etree.tostring(dom.xpath('.//div[@class="b module"]')[0])
@@ -53,7 +67,7 @@ def book_page(path):
         h2.append(E.br())
         return etree.tostring(h2)
     menu = make_menu(book, int(chapter))
-    content = render_template('base.html') + menu + content + menu
+    content = render_template('base.html', body=menu + content + menu)
     return content
 
 if __name__ == '__main__':
